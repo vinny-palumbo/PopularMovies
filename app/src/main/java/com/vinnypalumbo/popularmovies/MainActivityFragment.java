266f9fa9c330e77a35e4afcd0a98b0e42a94ef22
Vinny <vin_palumbo@hotmail.com>
2015-11-18 20:34:11 -0500

    Populate the GridView with dummy TextViews using an Adapter

--- app/src/main/java/com/vinnypalumbo/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/vinnypalumbo/popularmovies/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.vinnypalumbo.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,40 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] movieArray = {
+                "Batman",
+                "Spiderman",
+                "The Godfather",
+                "Forrest Gump",
+                "Pulp Fiction",
+                "The Goodfellas",
+                "Toy Story"
+        };
+
+        List<String> topMovies = new ArrayList<String>(
+                Arrays.asList(movieArray)
+        );
+
+        ArrayAdapter<String> mMovieAdapter = new ArrayAdapter<String>(
+                // Context: global info about app environment.
+                // Allows access the system services and resources
+                // We use the fragments containing the activity as the context
+                getActivity(),
+                // ID of grid item layout:
+                // The Adapter needs to know the layout for each grid item
+                R.layout.grid_item_movie,
+                // ID of text view within that layout that we want to populate
+                R.id.grid_item_movie_textview,
+                // List of data that we want to populate the text view with
+                topMovies
+        );
+
+        // Get a reference to the ListView, and attach the Adapter to it
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movie);
+        gridView.setAdapter(mMovieAdapter);
+
+        return rootView;
     }
 }
