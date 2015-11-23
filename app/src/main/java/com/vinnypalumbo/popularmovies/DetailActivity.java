b9049adb9e50afa64f18d2d6a7d67f74e22f2276
Vinny <vin_palumbo@hotmail.com>
2015-11-23 16:53:51 -0500

    Get the poster path from the intent and use it to populate the detail text

--- app/src/main/java/com/vinnypalumbo/popularmovies/DetailActivity.java
+++ app/src/main/java/com/vinnypalumbo/popularmovies/DetailActivity.java
@@ -12,8 +12,6 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
