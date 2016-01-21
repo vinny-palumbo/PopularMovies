d25f27e5ccb22c8c3fe6f483d97b262aaa5dfcf3
Vinny <vin_palumbo@hotmail.com>
2016-01-21 16:23:47 -0500

    Build Two Pane Tablet UI

--- app/src/main/java/com/vinnypalumbo/popularmovies/DetailActivity.java
+++ app/src/main/java/com/vinnypalumbo/popularmovies/DetailActivity.java
@@ -14,7 +14,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailActivityFragment())
+                    .add(R.id.movie_detail_container, new DetailActivityFragment())
                     .commit();
         }
     }
--- app/src/main/java/com/vinnypalumbo/popularmovies/MainActivity.java
+++ app/src/main/java/com/vinnypalumbo/popularmovies/MainActivity.java
@@ -8,20 +8,32 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = MainActivity.class.getSimpleName();
-    private final String MOVIEFRAGMENT_TAG = "MFTAG";
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
 
+    private boolean mTwoPane;
     private String mSort;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mSort = Utility.getPreferredSorting(this);
         super.onCreate(savedInstanceState);
+        mSort = Utility.getPreferredSorting(this);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MovieFragment(), MOVIEFRAGMENT_TAG)
-                    .commit();
+        if (findViewById(R.id.movie_detail_container) != null) {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // in two-pane mode.
+            mTwoPane = true;
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.movie_detail_container, new DetailActivityFragment(), DETAILFRAGMENT_TAG)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
         }
     }
 
@@ -54,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
         String sort = Utility.getPreferredSorting(this);
         // update the sorting in our second pane using the fragment manager
         if (sort != null && !sort.equals(mSort)) {
-            MovieFragment mf = (MovieFragment)getSupportFragmentManager().findFragmentByTag(MOVIEFRAGMENT_TAG);
-            if ( null != mf ) {
+            MovieFragment mf = (MovieFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_movie);
+            if (null != mf) {
                 mf.onSortChanged();
             }
             mSort = sort;
