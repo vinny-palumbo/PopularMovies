db1f0e864951599b8dc9077e1298b59d3650d7c8
Vinny <vin_palumbo@hotmail.com>
2016-01-26 14:02:56 -0500

    Delete old data before inserting new data when Sorting Preference is changed

--- app/src/main/java/com/vinnypalumbo/popularmovies/sync/PopularMoviesSyncAdapter.java
+++ app/src/main/java/com/vinnypalumbo/popularmovies/sync/PopularMoviesSyncAdapter.java
@@ -204,13 +204,15 @@ public class PopularMoviesSyncAdapter extends AbstractThreadedSyncAdapter {
             int inserted = 0;
             // add to database
             if ( cVVector.size() > 0 ) {
+                // delete old data so we don't build up an endless history
+                getContext().getContentResolver().delete(MovieContract.MovieEntry.CONTENT_URI, null, null);
                 // Student: call bulkInsert to add the movie entries to the movie database here
                 ContentValues[] cvArray = new ContentValues[cVVector.size()];
                 cVVector.toArray(cvArray);
                 getContext().getContentResolver().bulkInsert(MovieContract.MovieEntry.CONTENT_URI, cvArray);
             }
 
-            Log.d(LOG_TAG, "Fetch Movies Service Complete. " + inserted + " Inserted");
+            Log.d(LOG_TAG, "Sync Complete. " + inserted + " Inserted");
 
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage(), e);
