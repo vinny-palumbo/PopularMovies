9c92354ca52f97e4b0295968ea9daad81b47f3f7
Vinny <vin_palumbo@hotmail.com>
2016-02-05 21:49:13 -0500

    Remove useless options-related methods in Detail Activity

--- app/src/main/java/com/vinnypalumbo/popularmovies/DetailActivity.java
+++ app/src/main/java/com/vinnypalumbo/popularmovies/DetailActivity.java
@@ -1,11 +1,8 @@
 package com.vinnypalumbo.popularmovies;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -29,30 +26,5 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.movie_detail_container, fragment)
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Log.d("vinny-debug", "DetailActivity - onCreateOptionsMenu");
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Log.d("vinny-debug", "DetailActivity - onOptionsItemSelected");
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
